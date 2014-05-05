6fada23885d87280cb0e139044f2b243abd6ddb0
Philipp Scheit <p.scheit@ps-webforge.com>
2014-05-05 16:20:20 +0200

    remove compile test

--- bin/compile-test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require dirname(__DIR__).DIRECTORY_SEPARATOR.'bootstrap.php';
-
-$compiler = new Webforge\Process\Phar\Compiler();
-$compiler->compile(__DIR__.DIRECTORY_SEPARATOR.'webforge-process.phar', 'webforge-process.phar');
